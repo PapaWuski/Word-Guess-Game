5ab6772370a2af060008881c4138da923853580d
Peter Wu <23109224+PapaWuski@users.noreply.github.com>
2019-08-29 20:57:03 -0700

    added functions to  create and fill a board

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,4 +1,28 @@
-var listOfGames = ["megaman", "firefighter"];
+var listOfGames = [
+  "noctis lucis caelum",
+  "regis lucis caelum cxiii",
+  "lunafreya nox fleuret",
+  "gladiolus amicitia",
+  "ignis scientia",
+  "prompto argentum",
+  "ardyn izunia",
+  "iedolas aldercapt",
+  "ravus nox fleuret",
+  "verstael besithia",
+  "loqi and caligo",
+  "cor leonis",
+  "shiva",
+  "iris amicitia",
+  "cid and cindy",
+  "aranea highwind",
+  "nyx ulric",
+  "libertus ostium",
+  "titus drautos",
+  "somnus lucis caelum",
+  "gilgamesh",
+  "aera mirus fleuret",
+  "bahamut"
+];
 var win = 0;
 var lose = 0;
 var tries = 10;
@@ -7,10 +31,42 @@ var rightGuess = [];
 var screenGuess = document.getElementById("replacetext");
 var target = listOfGames[Math.floor(Math.random() * listOfGames.length)];
 var container = document.getElementById("tracker");
+var board = [];
+var targetDist = [...new Set(target)];
+console.log(target);
+console.log(targetDist);
+
+function resetBoard() {
+  board = [];
+  for (let i = 0; i < target.length; i++) {
+    if (target[i] === " ") {
+      board.push("&nbsp;");
+    } else {
+      board.push("_");
+    }
+  }
+  console.log(board);
+  if (targetDist.indexOf(" ") !== -1) {
+    targetDist.splice(targetDist.indexOf(" "), 1);
+  }
+  document.querySelector("#gametext").innerHTML = board.join(" ");
+}
+
+function appendLetter(letter) {
+  for (let i = 0; i < target.length; i++) {
+    if (target[i] === letter) {
+      board[i] = letter.toUpperCase();
+      document.querySelector("#gametext").innerHTML = board.join(" ");
+    }
+  }
+}
 
 function resetGame() {
-    target = listOfGames[Math.floor(Math.random() * listOfGames.length)];
+  target = listOfGames[Math.floor(Math.random() * listOfGames.length)];
+  targetDist = [...new Set(target)];
+  resetBoard();
   console.log(target);
+  console.log(targetDist);
   tries = 10;
   wrongGuess = [];
   rightGuess = [];
@@ -20,19 +76,17 @@ function resetGame() {
 
 function logScore(endGame) {
   var paragraph = document.createElement("P");
-  paragraph.textContent = `Game#: ${win + lose} |
-    ${endGame} | Goal: ${target} `;
+  paragraph.innerText = `Game#: ${win + lose}| ${endGame}
+                        Goal: ${target.toUpperCase()}
+                        Guesses Remaining: ${tries}`;
   if (endGame === "Win") {
     paragraph.setAttribute("class", "bg-primary text-light p-1");
   } else {
     paragraph.setAttribute("class", "bg-danger text-light p-1");
   }
 
-  container.appendChild(paragraph);
+  container.insertBefore(paragraph, container.firstChild);
 }
-console.log(target);
-var targetDist = [...new Set(target)];
-console.log(targetDist.join(" "));
 
 document.onkeypress = function(event) {
   // console.log(event.key);
@@ -45,17 +99,21 @@ document.onkeypress = function(event) {
     console.log("invalid letter");
     return;
   }
-  if (targetDist.includes(event.key) && !rightGuess.includes(event.key)) {
-    rightGuess.push(event.key);
+  var userInput = event.key.toLowerCase();
+  if (targetDist.includes(userInput) && !rightGuess.includes(userInput)) {
+    rightGuess.push(userInput);
+    appendLetter(userInput);
     console.log("right", rightGuess);
   } else if (
-    !wrongGuess.includes(event.key) &&
-    !rightGuess.includes(event.key)
+    !wrongGuess.includes(userInput) &&
+    !rightGuess.includes(userInput)
   ) {
     tries--;
-    wrongGuess.push(event.key);
+    wrongGuess.push(userInput);
     document.querySelector("#attempts").innerHTML = tries;
-    document.querySelector("#uiGuess").innerHTML = wrongGuess.join(" ");
+    document.querySelector("#uiGuess").innerHTML = wrongGuess
+      .join(" ")
+      .toUpperCase();
     console.log("wrong", wrongGuess);
     if (tries === 0) {
       lose++;
@@ -74,3 +132,4 @@ document.onkeypress = function(event) {
     resetGame();
   }
 };
+resetBoard();
